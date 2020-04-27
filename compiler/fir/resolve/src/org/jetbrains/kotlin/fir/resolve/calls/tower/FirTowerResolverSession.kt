dd354a997c24b3ae230341aa332a5756ef1f8057
Denis Zharkov <denis.zharkov@jetbrains.com>
2020-04-27 14:17:10 +0300

    FIR: Inline constructor parameter at FirTowerResolverSession

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/tower/FirTowerResolverSession.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/tower/FirTowerResolverSession.kt
@@ -35,8 +35,7 @@ import org.jetbrains.kotlin.resolve.descriptorUtil.HIDES_MEMBERS_NAME_LIST
 import org.jetbrains.kotlin.util.OperatorNameConventions
 
 class FirTowerResolverSession internal constructor(
-    val components: BodyResolveComponents,
-    implicitReceiverValues: List<ImplicitReceiverValue<*>>,
+    private val components: BodyResolveComponents,
     private val manager: TowerResolveManager,
     private val candidateFactoriesAndCollectors: CandidateFactoriesAndCollectors,
 ) {
@@ -52,7 +51,7 @@ class FirTowerResolverSession internal constructor(
     private val implicitReceiversUsableAsValues: List<ImplicitReceiver>
 
     init {
-        val (implicitReceivers, implicitReceiversUsableAsValues) = prepareImplicitReceivers(implicitReceiverValues)
+        val (implicitReceivers, implicitReceiversUsableAsValues) = prepareImplicitReceivers(components.implicitReceiverStack.receiversAsReversed())
         this.implicitReceivers = implicitReceivers
         this.implicitReceiversUsableAsValues = implicitReceiversUsableAsValues
     }
